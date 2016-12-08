f0050eaba6562bde58ef313eb0af1ccb3b2e8c2d
jirkakoutny <koutny.jiri@gmail.com>
2016-12-08 14:51:53 +0100

    Rest API refactoring

--- server/restapi.js
+++ server/restapi.js
@@ -1,7 +1,13 @@
+// nconf
+var nconf = require('nconf');
+nconf.argv().env().file('./config.json');
+
+// IoTHubClient
+var IoTHubClient = require('./iothub.js');
+
+// express
 var express = require('express')
 var bodyParser = require('body-parser');
-
-var IoTHubClient = require('./iothub.js');
 
 var app = express()
 
@@ -12,12 +18,13 @@ app.get('/api/get', function (req, res) {
     res.json({ message: 'Hello World!' })
 })
 
-app.post('/api/post', function (req, res) {
-    var token = req.body.token;
-    console.log(req.body.token);
-    IoTHubClient.sendC2DCommand(token);
-    res.json({ message: 'Got a POST request', token: token })
+app.post('/api/command', function (req, res) {
+    var targetDevice = nconf.get('targetDevice'); 
+    var action = req.body.action;
+    var actor = req.body.actor;
+    IoTHubClient.sendC2DCommand(targetDevice, action, actor);
+    res.json({ message: 'Got a POST request' })
 })
 
 app.listen(3001)
-
+console.log('RestAPI listening on port 3001');
