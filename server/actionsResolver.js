e1a8b4048c68aff625e635bc4addd2737d09b7f8
jirkakoutny <koutny.jiri@gmail.com>
2017-01-12 18:48:55 +0100

    Send parameters for actuators

--- server/actionsResolver.js
+++ server/actionsResolver.js
@@ -1,5 +1,30 @@
+// nconf
+var nconf = require('nconf');
+nconf.argv().env().file('./config.json');
+
 function resolveParametersFor(action, actor) {
-    return 0;
+   console.log('Resolving for: ' + action + ' ' + actor);
+   
+   var result = nconf.get(actor+action); 
+
+   console.log('Result: ' + result);
+
+    if(actor == 'lights')
+    {
+        if(action == 'switchOn')
+            {
+                return "13988876";
+            }
+            
+            if(action == 'switchOff')
+            {
+                return "13988867";
+            }
+    }
+    else 
+    {
+        return "";
+    }
 }
 
 module.exports = {
